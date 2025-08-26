c0b3b30e43855e428387971492c6c5ebde1cbc7d
Sonny-315 <sys_315@naver.com>
2025-08-26 17:52:37 +0900

    초기 커밋.

---
+++ CloneExplorer/App.cs
@@ -0,0 +1,18 @@
+﻿using Jamesnet.Wpf.Controls;
+using System.Windows;
+
+namespace CloneExplorer
+{
+    // internal => 해당 클래스가 프로젝트 내부에서만 사용되며, 외부 접근 허용 X
+    // App Class는 해당 애플리케이션의 핵심 구성 및 동작 담당
+    internal class App : JamesApplication
+    {
+        protected override Window CreateShell()
+        {
+            return new Window();
+        }
+
+
+
+    }
+}
---
+++ CloneExplorer/Starter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneExplorer
+{
+    internal class Starter
+    {
+        // - 프로그램 시작 진입점 역할
+        // - App 인스턴스를 생성하고 실행하는 것이 주 목적!
+        // - 프로그램의 시작 지점을 만들기 위해서는 STAThread 어트리뷰트가 포함된 메서드를 만들면 됨.
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            _ = new App().Run();
+        }
+
+
+    }
+}
